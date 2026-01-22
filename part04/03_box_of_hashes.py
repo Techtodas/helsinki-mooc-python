a09ab02147a5f4ae6004ab8606d43ba6b58515e3
Techtodas <jeffersonpullido@gmail.com>
2026-01-22 14:56:46 +0800

    Part 4: Complete exercise 03

---
+++ part04/03_box_of_hashes.py
@@ -0,0 +1,19 @@
+# Copy here code of line function from previous exercise
+
+def box_of_hashes(height):
+    # You should call function line here with proper parameters
+    counter = 0
+    while counter < height:
+        line(10, "#")
+        counter+=1
+
+def line(number, text):
+    if len(text) == 0:
+        print("*"*number)
+    elif len(text) > 1 and text[1] == "":
+        print
+    else:
+        print(text[0]*number)
+# You can test your function by calling it within the following block
+if __name__ == "__main__":
+    box_of_hashes(5)
