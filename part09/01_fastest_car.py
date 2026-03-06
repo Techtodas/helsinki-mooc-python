81c00276ef7abda1b98a690e5854d8e35e4fe6dc
Techtodas <jeffersonpullido@gmail.com>
2026-03-06 15:37:46 +0800

    Add MOOC Helsinki Part 9 exercises

---
+++ part09/01_fastest_car.py
@@ -0,0 +1,28 @@
+# Write your solution after the class Car
+# Do not make changes to the class!
+class Car:
+    def __init__(self, make: str, top_speed: int):
+        self.make = make
+        self.top_speed = top_speed
+
+    def __str__(self):
+        return f"Car (make: {self.make}, top speed: {self.top_speed})"
+
+# WRITE YOUR SOLUTION HERE:
+def fastest_car(cars: list):
+    fast_car = cars[0].top_speed
+    for car in cars:
+        if car.top_speed > fast_car:
+            fast_car = car.top_speed
+            fast_car_name = car.make
+            return fast_car_name
+            
+
+if __name__ == "__main__":
+    car1 = Car("Saab", 195)
+    car2 = Car("Lada", 110)
+    car3 = Car("Ferrari", 280)
+    car4 = Car("Trabant", 85)
+
+    cars = [car1, car2, car3, car4]
+    print(fastest_car(cars))
